747fb5d4d61b6d17cc24e2f29b6455686f9cfcfc
kosukeKK <koskacts@gmail.com>
2018-05-04 21:24:07 +0900

    article作成ユーザーの表示を→に移動した

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -6,19 +6,20 @@
         <div class="article-list">
             <div>
                 <h2>{{$article->title}}</h2>
-                {{"@".$article->user->name}}さん<br>
-                text:{{$article->text}}
+                {{$article->text}}
             </div>
-            @if(isset($loginUser) && $article->user_id == $loginUser->id)
-                <div>
+
+            <div>
+                {{"@".$article->user->name}}<br>
+                @if(isset($loginUser) && $article->user_id == $loginUser->id)
                     <a href="{{url("article/$article->id/edit")}}" type="button" class="edit-button">edit</a>
                     <form method="post" action="{{url("article/$article->id")}}">
                         {{ csrf_field() }}
                         <input name="_method" type="hidden" value="DELETE">
                         <input type="submit" value="delete">
                     </form>
-                </div>
-            @endif
+                @endif
+            </div>
         </div>
     @endforeach
     <div class="pagenate">{{ $articles->links() }}</div>
